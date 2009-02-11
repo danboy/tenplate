f7622909887bf33842aa2f6160d9b2e033b56551
Tom Kersten <tkersten@obtiva.com>
2009-02-10 20:35:13 -0600

    Small updates to allow choice between symlinking or copying sass files...a bit hacky...but...sufficient for the purpose at this point.

--- lib/tenplate/cli.rb
+++ lib/tenplate/cli.rb
@@ -10,17 +10,30 @@ module Tenplate
 
       Dir.mkdir(destination_dir) unless File.exist?(destination_dir)
 
+      question_text = "Do you want to copy or symlink the tenplate sass files? [c/s]: "
+      copy_or_symlink = ask(question_text, lambda {|answer| answer.match(/^s/i) ? :symlink : :copy }) do |response|
+        response.validate = /^[csCS]/
+      end
+
       # Add public/stylesheets/sass directory and symlink to included SASS files
       Dir.open(source_dir).each do |f|
-        next if f == ".." || f == "."
-        File.safe_unlink("#{destination_dir}/#{f}") if File.symlink?("#{destination_dir}/#{f}")
+        destination_path = "#{destination_dir}/#{f}"
+        source_path      = "#{source_dir}/#{f}"
 
-        if File.exist?("#{destination_dir}/#{f}") && agree("'#{f}' already exists in '#{destination_dir}'. Do you want to delete it? ")
-          File.delete("#{destination_dir}/#{f}")
+        next if f == ".." || f == "."
+        File.safe_unlink(destination_path) if File.symlink?(destination_path)
+
+        if File.exists?(destination_path) && deletion_confirmed?(destination_path)
+          File.delete(destination_path)
         end
-        File.symlink("#{source_dir}/#{f}", "#{destination_dir}/#{f}")
+        File.send(copy_or_symlink, source_path, destination_path)
       end
       puts "Your sass has been tenplated..."
     end
+
+    private
+      def self.deletion_confirmed?(file)
+        agree("'#{file}' already exists. Do you want to delete it? ")
+      end
   end
 end
