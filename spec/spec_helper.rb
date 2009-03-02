2039fe06e46331e22166a7eab7ee6b59461cd128
Tom Kersten <tkersten@obtiva.com>
2009-03-02 08:59:19 -0600

    Included ActiveRecord in order to test auto-rendering checkboxes & radio buttons

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'spec'
 gem 'ruby-debug'
 gem 'actionpack'
 require 'action_view'
+require 'activerecord'
 require '/Users/tom/tenplate/lib/templates/form_builder/tenplate_form_builder'
 
 Spec::Runner.configure do |config|
