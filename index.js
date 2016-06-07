14a06451b1c86b9bf7da0b8fd9471a36a9d1e0ac
Dan Nawara <dan@pinmonkey.com>
2016-06-07 13:34:31 -0500

    add custm controller paths

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 var fs = require('fs');
 var self = {
   controllers: function(options){
-    var path = __dirname+'/../../app/controllers/'
-    , files = fs.readdirSync(path)
-    , Controllers = {};
+    var path = (options && options.path) ? options.path : __dirname+'/../../app/controllers/';
+    
+    options = Object.assign({
+      path : path 
+    }, options);
+
+    var files = fs.readdirSync(options.path),
+        Controllers = {};
+
+    files.forEach(function(controller){
+      if (controller === "index.js" || controller.substr(controller.lastIndexOf('.') + 1) !== 'js')
+        return;
+      var name = controller.charAt(0).toUpperCase()+controller.replace('.js', '').replace('Controller','').slice(1);
+      var c = require(options.path+controller);
+      Controllers[name] = c;
+    });
+
+    return Controllers;
+  },
+  getControllers: function(options){
+    options = Object.assign({
+      path : __dirname+'/../../app/controllers/'
+    }, options);
+
+    var files = fs.readdirSync(options.path),
+        Controllers = {};
 
     files.forEach(function(controller){
       if (controller === "index.js" || controller.substr(controller.lastIndexOf('.') + 1) !== 'js')
@@ -15,10 +38,9 @@ var self = {
 
     return Controllers;
   },
-
   locals: function(app){
-    var path = __dirname+'/../../app/helpers/'
-    , files = fs.readdirSync(path);
+    var path = __dirname+'/../../app/helpers/',
+        files = fs.readdirSync(path);
 
     files.forEach(function(controller){
       if (controller === "index.js" || controller.substr(controller.lastIndexOf('.') + 1) !== 'js')
@@ -26,7 +48,7 @@ var self = {
       var locals = require(path+controller);
       for(var key in locals){
         app.set(key, locals[key]);
-      };
+      }
     });
 
   },
